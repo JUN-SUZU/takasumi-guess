8a10322302efa22e9519c1c43ff0a1e2ee4b9e88
JUN-SUZU <jun2006.master@gmail.com>
2024-09-25 19:08:46 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function calc() {
+    let base = document.getElementById('base').value;
+    let molecule = document.getElementById('molecule').value;
+    let denominator = document.getElementById('denominator').value;
+    let money = document.getElementById('money').value;
+    let result = document.getElementById('result');
+    if (molecule <= denominator) {
+        result.innerHTML = "分子は分母より大きい値を入力してください";
+        return;
+    }
+    let losses = base * 3 / 2;
+    let resText = "1: " + base + "<br>";
+    base *= (molecule - denominator) / denominator;
+    base = Math.floor(base);
+    resText += "2: " + base + "<br>";
+    // log(底) {真数}
+    // log(molecule/denominator) {2*molecule*money/3*denominator*base}
+    let n = Math.floor(Math.log(2 * molecule * money / 3 / denominator / base) / Math.log(molecule / denominator));
+    for (let i = 2; i < n; i++) {
+        base *= molecule / denominator;
+        base = Math.floor(base);
+        resText += (i + 1) + ": " + base + "<br>";
+    }
+    for (let i = 2; i < n; i++) {
+        losses *= molecule / denominator;
+        losses = Math.floor(losses);
+    }
+    resText += "連続で負けることができる回数: " + (n - 1) + "回<br>その時の負け金: " + losses + "円";
+    result.innerHTML = resText;
+}
