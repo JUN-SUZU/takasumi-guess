d01f97a6cd884f6ffb5c0c5a02e9f42527834b44
JUN-SUZU <jun2006.master@gmail.com>
2024-09-26 13:00:21 +0900

    add: plosses

--- index.js
+++ index.js
@@ -9,22 +9,32 @@ function calc() {
         return;
     }
     let losses = base * 3 / 2;
+    let plosses = losses;
     let u = 3 / 2;
     let v = molecule / denominator;
     // let n = [(log(money)-log(u*base))/log(1+uv)]+1
     let n = Math.floor((Math.log(money) - Math.log(u * base)) / Math.log(1 + u * v)) + 1;
-    let text = `1: ${base}<br>`;
+    let text = `1: ${base} 負けた場合: ${Math.floor(plosses)}<br>`;
     base *= (u * v);
     for (let i = 0; i < n - 1; i++) {
         let x = base;
         for (let j = 0; j < i; j++) {
             x *= (1 + u * v);
         }
-        text += `${i + 2}: ${Math.floor(x)}<br>`;
+        plosses += x;
+        text += `${i + 2}: ${Math.floor(x)} 負けた場合: ${Math.floor(plosses)}<br>`;
     }
     for (let i = 0; i < n - 1; i++) {
         losses *= (1 + u * v);
     }
+    let percentage = 100;
+    for (let i = 0; i < n; i++) {
+        percentage *= 2;
+    }
+    for (let i = 0; i < n; i++) {
+        percentage /= 3;
+    }
     text += `損失: ${Math.floor(losses)}<br>`;
-    text += `継続できる回数: ${n}`;
+    text += `継続できる回数: ${n}回<br>`;
+    text += `すべてを失う確率: ${percentage}%<br>`;
 }
